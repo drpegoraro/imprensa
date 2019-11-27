420b6963857ff3befe2b78c21c8bbc86807ad59b
drpegoraro <56133734+drpegoraro@users.noreply.github.com>
2019-11-27 17:05:04 -0300

    Create imprensa.sol

---
+++ contrato/imprensa.sol
@@ -0,0 +1,28 @@
+pragma solidity 0.5.13;
+
+contract imprensa
+
+{
+    constructor
+    (
+    string memory NomeJornalista,
+    string memory  WebSite,
+    uint256 DataPublicacao
+    )
+    public
+    
+    Jornalista= NomeJornalista;
+    Site=WebSite;
+    Data=DataPublicacao;
+    Materia=MateriaJornalistica;
+    
+    struct
+    string memory MateriaJornalistica;
+    string memory PesquisaMateria;
+    
+    function PesquisaMateriaJornalistica () return view (MateriaJornalistica)
+    {
+        mapping (PesquisaMateria);
+        returns
+    }
+}
